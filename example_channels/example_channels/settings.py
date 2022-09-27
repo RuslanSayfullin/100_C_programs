0674fd205346ef4f62665eac8516ebf94771a38c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-27 17:12:43 +0500

    Функционал для аутентификаций пользователя.

--- example_channels/example_channels/settings.py
+++ example_channels/example_channels/settings.py
@@ -22,8 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'channels'
     'example.apps.ExampleConfig',
+    'channels',
 ]
 
 MIDDLEWARE = [
