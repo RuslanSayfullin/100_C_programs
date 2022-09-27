d6cef3496ca4a769a66f8b2be08e2afe7e892f6a
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-27 17:18:07 +0500

    Login Alerts.

--- example_channels/example/views.py
+++ example_channels/example/views.py
@@ -2,10 +2,24 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.decorators import login_required
+
+User = get_user_model()
 
 
+@login_required(login_url='/log_in/')
 def user_list(request):
-    return render(request, 'example/user_list.html')
+    """
+    NOTE: This is fine for demonstration purposes, but this should be
+    refactored before we deploy this app to production.
+    Imagine how 100,000 users logging in and out of our app would affect
+    the performance of this code!
+    """
+    users = User.objects.select_related('logged_in_user')
+    for user in users:
+        user.status = 'Online' if hasattr(user, 'logged_in_user') else 'Offline'
+    return render(request, 'example/user_list.html', {'users': users})
 
 
 def log_in(request):
@@ -20,6 +34,7 @@ def log_in(request):
     return render(request, 'example/log_in.html', {'form': form})
 
 
+@login_required(login_url='/log_in/')
 def log_out(request):
     logout(request)
     return redirect(reverse('example:log_in'))
