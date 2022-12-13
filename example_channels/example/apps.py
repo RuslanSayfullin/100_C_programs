64a907ccc9065002d0b38a25c10ddb619f1e9aba
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-12-13 11:18:09 +0500

    Очистили проект.

--- example_channels/example/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExampleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'example'
-
-    def ready(self):
-        import example.signal
--- example_channels/example/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from example.views import user_list, log_in, log_out, sign_up
-
-urlpatterns = [
-    re_path(r'^log_in/$', log_in, name='log_in'),
-    re_path(r'^log_out/$', log_out, name='log_out'),
-    re_path(r'^sign_up/$', sign_up, name='sign_up'),
-    re_path(r'^$', user_list, name='user_list')
-]
