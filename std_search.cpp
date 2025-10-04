a5d944e092977b4eff57660050bcc47b82d4d34d
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-10-04 11:54:25 +0500

    Функция std::search используется для поиска последовательности элементов в другой последовательности.

---
+++ std_search.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main() {
+    std::vector<int> haystack = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<int> needle = {5, 6, 7};
+
+    auto it = std::search(haystack.begin(), haystack.end(), needle.begin(), needle.end());
+
+    if (it != haystack.end()) {
+        std::cout << "Подпоследовательность найдена, начинаая с индекса: " << std::distance(haystack.begin(), it) << std::endl;
+    } else {
+        std::cout << "подпоследовательность не найдена." << std::endl;
+    }
+    return 0;
+}
