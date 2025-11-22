e9130c36ee6fe73d5a7210dec3ffc1d4d033f460
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-11-22 23:28:50 +0500

    Алгоритм переставляет элементы так, чтобы получилась предыдущая в лексикографическом порядке перестановка. Можно применять не только к векторам, но и к строкам (как и многие другие алгоритмы).

---
+++ prev_permutation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+int main() {
+    std::string s="abc";
+    std::sort(s.begin(), s.end(), std::greater<char>());
+    do {
+        std::cout << s << ' ';
+    } while(std::prev_permutation(s.begin(), s.end()));
+
+    std::cout << '\n';
+
+    return 0;
+}
