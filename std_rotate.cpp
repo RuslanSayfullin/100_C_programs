73594d597d00040d1d7c6cf9cc5f6bf451418132
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-12-21 23:51:32 +0500

    Меняет местами элементы в диапазоне [first, last) таким образом, что элемент n_first становится первым в новом диапазоне, а n_first-1 — последним.

---
+++ std_rotate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<algorithm>
+#include <vector>
+
+int main() {
+    std::vector<int> myvector;
+
+    // set some values:
+    for(int i=1; i<10; ++i) myvector.push_back(i);  // 1 2 3 4 5 6 7 8 9
+
+    std::rotate(myvector.begin(), myvector.begin()+3, myvector.end());
+
+    // print out content:
+    std::cout << "myvector contains: ";
+    for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+
+    return 0;
+}
