d9b7e732ece6e335f9d2353c0b3bfe2da6c9e8e0
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-12-27 20:40:49 +0500

    Это функция C++17, которая предоставляет безопасный с точки зрения типов контейнер для единичных значений любого типа.

---
+++ std_any.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <any>
+
+int main() {
+    std::any val;
+
+    val = 10;
+    if (val.type() == typeid(int)) {
+        std::cout << std::any_cast<int>(val) << std::endl;
+    }
+
+    val = 3.14f;
+    if (val.type() == typeid(float)) {
+        std::cout << std::any_cast<float>(val) << std::endl;
+    }
+
+    val = std::string("Hello, world!");
+    if (val.type() == typeid(std::string)) {
+        std::cout << std::any_cast<std::string>(val) << std::endl;
+    }
+
+    try {
+        std::cout << std::any_cast<int>(val) << std::endl;
+    } catch (const std::bad_any_cast& ex) {
+        std::cout << "Exception: " << ex.what() << std::endl;
+    }
+
+    return 0;
+}
